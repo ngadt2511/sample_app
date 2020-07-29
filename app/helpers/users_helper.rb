a94b03b5045968255dcf3909c0f90c167171571c
ngadt2511 <do.thuy.nga@sun-asterisk.com>
2020-07-29 11:45:55 +0700

    Chapter 10

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,8 +1,12 @@
 module UsersHelper
   # Returns the Gravatar for the given user.
   def gravatar_for user, size: Settings.helpers.gravatar.size
-    gravatar_id = Digest.MD5.hexdigest(user.email.downcase)
+    gravatar_id = Digest::MD5.hexdigest(user.email.downcase)
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
     image_tag gravatar_url, alt: user.name, class: "gravatar"
+  end
+
+  def current_user? user
+    user && user == current_user
   end
 end
