1236184d3fe106a5268463b0aa21e7b2b3456dbe
ngadt2511 <do.thuy.nga@sun-asterisk.com>
2020-08-03 09:53:56 +0700

    Chapter 9

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,7 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = Settings.validation.email.regex
   USERS_PARAMS = %i(name email password password_confirmation).freeze
+  attr_accessor :remember_token
 
   validates :name, presence: true,
     length: {maximum: Settings.validation.name.max_length}
@@ -14,6 +15,33 @@ class User < ApplicationRecord
   before_save :email_downcase
 
   has_secure_password
+
+  class << self
+    def digest string
+      cost =
+        if ActiveModel::SecurePassword.min_cost
+          BCrypt::Engine::MIN_COST
+        else
+          BCrypt::Engine.cost
+        end
+      BCrypt::Password.create string, cost: cost
+    end
+  end
+
+  def remember
+    self.remember_token = User.new_token
+    update :remember_digest, User.digest(remember_token)
+  end
+
+  def authenticated? remember_token
+    return false if remember_digest.nil?
+
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
+
+  def forget
+    update :remember_digest, nil
+  end
 
   private
 
